30bef2ac4528f41cb5b57afe88ee950560b0d699
Sand <4gFbo16q72w@mail.ru>
2023-06-08 20:52:53 +0700

    again?

---
+++ thirdparty/test_1.c
@@ -0,0 +1,18 @@
+#include <../../src/lib/functions.h>
+#include "ctest.h"
+
+CTEST(functions_test, functions)
+{
+    
+    int p_true, r=40;
+    
+    format(&p_true);
+
+    int expected_p = r;
+  
+
+    // Then
+   
+   ASSERT_DBL_NEAR_TOL(expected_p, p_true, 1);
+}
+
