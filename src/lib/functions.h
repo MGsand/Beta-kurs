c52c25c25c2f138aa44e9b39a16980d66d63789a
Sand <4gFbo16q72w@mail.ru>
2023-06-08 20:49:00 +0700

    new header

---
+++ src/lib/functions.h
@@ -0,0 +1,25 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void format(int ds);
+
+void noformat(int ds);
+
+void paragraph();
+
+void selectfail();
+
+void Clean();
+
+void Cleanabz();
+
+void save();
+
+void printsave();
+
+void strlong();
